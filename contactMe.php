e2a1b887361941fda01e7669a9cd549c21cfdd2e
petitcroco <58482685+p3titcr0c0@users.noreply.github.com>
2020-01-15 20:39:22 +0100

    Create contactMe.php

---
+++ contactMe.php
@@ -0,0 +1,30 @@
+<?php
+
+if (isset($_POST['submit'])) {
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['mail'];	
+$message = $_POST['message'];
+
+$mailTo = "admin@petitcroco.fr";
+$headers = "From: ".$mailFrom;
+$txt = "Vous avez reçu un email de ".$name.":\n\n".$message;
+
+if (($name != '') && ($subject != '') && ($mailFrom != '') && ($message != '')){
+	if (strstr($mailFrom, "@") !== FALSE) {
+		if (strstr($mailFrom, ".") !== FALSE) {
+			mail($mailTo, $subject, $txt, $headers);
+			header("Location: ../contact-success.html");
+		}
+		else{
+		header("Location: ../contact-fail.html");
+		}
+	}
+	else{
+	header("Location: ../contact-fail.html");
+	}
+}
+else{
+	header("Location: ../contact-fail.html");
+}
+}
